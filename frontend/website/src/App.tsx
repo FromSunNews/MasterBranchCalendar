af22428b5777fe40fd1a3a4913adccecec956275
FromSunNews <phuongtu590@gmail.com>
2024-04-17 17:15:55 +0700

    struture for my project

---
+++ frontend/website/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-xl font-bold">Hello Vite + React!</h1>
+      <h2>Typescript is the best!</h2>
+    </>
+  );
+}
+
+export default App;
